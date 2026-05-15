7efcf9b595e16009c68adc13dc276c26c45842b1
Siyamthanda Bulungu <26033452@mvula.univen.ac.za>
2026-05-15 13:56:25 +0200

    First commit

---
+++ src/main/java/com/mycompany/silent02/SILENT02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.silent02;
+
+/**
+ *
+ * @author 26033452
+ */
+public class SILENT02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
